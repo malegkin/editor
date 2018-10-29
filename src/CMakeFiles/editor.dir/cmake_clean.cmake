ba3d6a99c14613f492de178d3e7528b370518730
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-29 20:25:15 +0000

    Deploy malegkin/editor to github.com/malegkin/editor.git:gh-pages

---
+++ src/CMakeFiles/editor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/editor.dir/main.cpp.o"
+  "editor.pdb"
+  "editor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+
+
+
+int main()
+{
+    try {
+
+    }
+    catch (std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return -1;
+    }
+    catch (...) {
+        std::cerr << "Caught unknown exception." << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
